04b1f5759e0579819c04d3df4a22c17b8d4cf66b
Andrian Zubko <ondr@mail.ru>
2023-09-26 15:24:32 +0300

    -url in paginator

--- src/Lazy/Sys/Paginator.php
+++ src/Lazy/Sys/Paginator.php
@@ -19,21 +19,12 @@ class Paginator extends \SFW\Lazy\Sys
     /**
      * Overlaying paginator class.
      */
-    public function calc(int $totalEntries, int $entriesPerPage, int $pagesPerSet, int $currentPage): array
-    {
-        $pagination = (new \SFW\Paginator(...func_get_args()))->toArray();
-
-        $pagination['url'] = preg_replace('/[&?]i=[^&?]*/u', '',
-            $_SERVER['REQUEST_URI']
-        );
-
-        $pagination['url'] .= sprintf('%s%s=',
-            str_contains($pagination['url'], '?')
-                ? '&' : '?',
-
-            self::$config['sys']['paginator']['param']
-        );
-
-        return $pagination;
+    public function calc(
+        int $totalEntries,
+        int $entriesPerPage,
+        int $pagesPerSet,
+        int $currentPage
+    ): array {
+        return (new \SFW\Paginator(...func_get_args()))->toArray();
     }
 }

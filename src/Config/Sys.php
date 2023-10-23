4e892f5a3825b64afa2a8422272a36ea366e3c2f
Andrian Zubko <ondr@mail.ru>
2023-10-23 14:07:24 +0300

    new config scheme fix

--- src/Config/Sys.php
+++ src/Config/Sys.php
@@ -12,7 +12,7 @@ abstract class Sys extends \SFW\Config
      */
     protected static function defaults(): array
     {
-        $sys = [];
+        $config = [];
 
         // {{{ access control
 
@@ -20,7 +20,7 @@ abstract class Sys extends \SFW\Config
          *
          * array
          */
-        $sys['shared'] = [];
+        $config['shared'] = [];
 
         // }}}
         // {{{ general
@@ -29,25 +29,25 @@ abstract class Sys extends \SFW\Config
          *
          * string 'dev', 'test', 'prod', etc..
          */
-        $sys['env'] = 'dev';
+        $config['env'] = 'dev';
 
         /* Debug mode (not minify HTML/CSS/JS if true).
          *
          * bool
          */
-        $sys['debug'] = false;
+        $config['debug'] = false;
 
         /* Basic url (autodetect if null).
          *
          * ?string
          */
-        $sys['url'] = null;
+        $config['url'] = null;
 
         /* Default timezone.
          *
          * string
          */
-        $sys['timezone'] = 'UTC';
+        $config['timezone'] = 'UTC';
 
         // }}}
         // {{{ databaser
@@ -56,57 +56,57 @@ abstract class Sys extends \SFW\Config
          *
          * string 'Pgsql' or 'Mysql'
          */
-        $sys['db_default'] = 'Pgsql';
+        $config['db_default'] = 'Pgsql';
 
         /**
          * Pgsql.
          */
-        $sys['db_pgsql_host'] = 'localhost';
+        $config['db_pgsql_host'] = 'localhost';
 
-        $sys['db_pgsql_port'] = 5432;
+        $config['db_pgsql_port'] = 5432;
 
-        $sys['db_pgsql_db'] = null;
+        $config['db_pgsql_db'] = null;
 
-        $sys['db_pgsql_user'] = null;
+        $config['db_pgsql_user'] = null;
 
-        $sys['db_pgsql_pass'] = null;
+        $config['db_pgsql_pass'] = null;
 
-        $sys['db_pgsql_persistent'] = false;
+        $config['db_pgsql_persistent'] = false;
 
-        $sys['db_pgsql_charset'] = 'utf-8';
+        $config['db_pgsql_charset'] = 'utf-8';
 
-        $sys['db_pgsql_mode'] = \SFW\Databaser::ASSOC;
+        $config['db_pgsql_mode'] = \SFW\Databaser::ASSOC;
 
         /**
          * Mysql.
          */
-        $sys['db_mysql_host'] = 'localhost';
+        $config['db_mysql_host'] = 'localhost';
 
-        $sys['db_mysql_port'] = 3306;
+        $config['db_mysql_port'] = 3306;
 
-        $sys['db_mysql_db'] = null;
+        $config['db_mysql_db'] = null;
 
-        $sys['db_mysql_user'] = null;
+        $config['db_mysql_user'] = null;
 
-        $sys['db_mysql_pass'] = null;
+        $config['db_mysql_pass'] = null;
 
-        $sys['db_mysql_persistent'] = false;
+        $config['db_mysql_persistent'] = false;
 
-        $sys['db_mysql_charset'] = 'utf8mb4';
+        $config['db_mysql_charset'] = 'utf8mb4';
 
-        $sys['db_mysql_mode'] = \SFW\Databaser::ASSOC;
+        $config['db_mysql_mode'] = \SFW\Databaser::ASSOC;
 
         /* Log slow queries.
          *
          * ?string
          */
-        $sys['db_slow_queries_log'] = APP_DIR . '/var/log/slow.queries.log';
+        $config['db_slow_queries_log'] = APP_DIR . '/var/log/slow.queries.log';
 
         /* Log slow queries with minimal time in seconds.
          *
          * float
          */
-        $sys['db_slow_queries_min'] = 0.5;
+        $config['db_slow_queries_min'] = 0.5;
 
         // }}}
         // {{{ transaction
@@ -115,13 +115,13 @@ abstract class Sys extends \SFW\Config
          *
          * int
          */
-        $sys['transaction_retries'] = 7;
+        $config['transaction_retries'] = 7;
 
         /* Log transactions fails.
          *
          * ?string
          */
-        $sys['transaction_fails_log'] = APP_DIR . '/var/log/transaction.fails.log';
+        $config['transaction_fails_log'] = APP_DIR . '/var/log/transaction.fails.log';
 
         // }}}
         // {{{ cacher
@@ -130,33 +130,33 @@ abstract class Sys extends \SFW\Config
          *
          * string 'Nocache', 'Apc', 'Memcached' or 'Redis'
          */
-        $sys['cacher_default'] = 'Apc';
+        $config['cacher_default'] = 'Apc';
 
         /* Apc.
          */
-        $sys['cacher_apc_ttl'] = 3600;
+        $config['cacher_apc_ttl'] = 3600;
 
-        $sys['cacher_apc_ns'] = null;
+        $config['cacher_apc_ns'] = null;
 
         /* Memcached.
          */
-        $sys['cacher_memcached_ttl'] = 3600;
+        $config['cacher_memcached_ttl'] = 3600;
 
-        $sys['cacher_memcached_ns'] = null;
+        $config['cacher_memcached_ns'] = null;
 
-        $sys['cacher_memcached_servers'] = [['127.0.0.1', 11211]];
+        $config['cacher_memcached_servers'] = [['127.0.0.1', 11211]];
 
-        $sys['cacher_memcached_options'] = null;
+        $config['cacher_memcached_options'] = null;
 
         /* Redis.
          */
-        $sys['cacher_redis_ttl'] = 3600;
+        $config['cacher_redis_ttl'] = 3600;
 
-        $sys['cacher_redis_ns'] = null;
+        $config['cacher_redis_ns'] = null;
 
-        $sys['cacher_redis_connect'] = ['127.0.0.1', 6379, 2.5];
+        $config['cacher_redis_connect'] = ['127.0.0.1', 6379, 2.5];
 
-        $sys['cacher_redis_options'] = null;
+        $config['cacher_redis_options'] = null;
 
         // }}}
         // {{{ templater
@@ -165,29 +165,29 @@ abstract class Sys extends \SFW\Config
          *
          * string 'Native', 'Twig' or 'Xslt'
          */
-        $sys['templater_default'] = 'Native';
+        $config['templater_default'] = 'Native';
 
         /* Native
          */
-        $sys['templater_native_dir'] = APP_DIR . '/templates';
+        $config['templater_native_dir'] = APP_DIR . '/templates';
 
-        $sys['templater_native_minify'] = false;
+        $config['templater_native_minify'] = false;
 
         /* Twig
          */
-        $sys['templater_twig_dir'] = APP_DIR . '/templates';
+        $config['templater_twig_dir'] = APP_DIR . '/templates';
 
-        $sys['templater_twig_cache'] = APP_DIR . '/var/cache/twig';
+        $config['templater_twig_cache'] = APP_DIR . '/var/cache/twig';
 
-        $sys['templater_twig_strict'] = true;
+        $config['templater_twig_strict'] = true;
 
         /* Xslt
          */
-        $sys['templater_xslt_dir'] = APP_DIR . '/templates';
+        $config['templater_xslt_dir'] = APP_DIR . '/templates';
 
-        $sys['templater_xslt_root'] = 'root';
+        $config['templater_xslt_root'] = 'root';
 
-        $sys['templater_xslt_item'] = 'item';
+        $config['templater_xslt_item'] = 'item';
 
         // }}}
         // {{{ notifier
@@ -198,25 +198,25 @@ abstract class Sys extends \SFW\Config
          *
          * bool
          */
-        $sys['notifier_enabled'] = true;
+        $config['notifier_enabled'] = true;
 
         /* Instead of disabling, you can override recipients by your email.
          *
          * ?array('EMAIL' or array('EMAIL'[, 'NAME']), ...)
          */
-        $sys['notifier_recipients'] = null;
+        $config['notifier_recipients'] = null;
 
         /* Default sender.
          *
          * 'EMAIL' or array('EMAIL'[, 'NAME']) or null
          */
-        $sys['notifier_sender'] = null;
+        $config['notifier_sender'] = null;
 
         /* Default replies.
          *
          * array('EMAIL' or array('EMAIL'[, 'NAME']), ...)
          */
-        $sys['notifier_replies'] = [];
+        $config['notifier_replies'] = [];
 
         // }}}
         // {{{ locker
@@ -225,7 +225,7 @@ abstract class Sys extends \SFW\Config
          *
          * string
          */
-        $sys['locker_file'] = APP_DIR . '/var/locks/{KEY}.lock';
+        $config['locker_file'] = APP_DIR . '/var/locks/{KEY}.lock';
 
         // }}}
         // {{{ logger
@@ -234,7 +234,7 @@ abstract class Sys extends \SFW\Config
          *
          * ?string
          */
-        $sys['logger_file'] = APP_DIR . '/var/log/errors.log';
+        $config['logger_file'] = APP_DIR . '/var/log/errors.log';
 
         // }}}
         // {{{ response
@@ -245,7 +245,7 @@ abstract class Sys extends \SFW\Config
          *
          * ?array
          */
-        $sys['response_compress_mimes'] = [
+        $config['response_compress_mimes'] = [
             'text/html',
             'text/plain',
             'text/xml',
@@ -261,19 +261,19 @@ abstract class Sys extends \SFW\Config
          *
          * int
          */
-        $sys['response_compress_min'] = 32 * 1024;
+        $config['response_compress_min'] = 32 * 1024;
 
         /* Optional error document files pattern.
          *
          * ?string
          */
-        $sys['response_error_document'] = APP_DIR . '/public/.bin/errors/{CODE}.html.php';
+        $config['response_error_document'] = APP_DIR . '/public/.bin/errors/{CODE}.html.php';
 
         /* Pattern for statistics to each html page generated by templater.
          *
          * ?string
          */
-        $sys['response_stats'] = '<!-- script {SCR_T} + sql({SQL_C}) {SQL_T} + tpl({TPL_C}) {TPL_T} = {ALL_T} -->';
+        $config['response_stats'] = '<!-- script {SCR_T} + sql({SQL_C}) {SQL_T} + tpl({TPL_C}) {TPL_T} = {ALL_T} -->';
 
         // }}}
         // {{{ dir
@@ -282,7 +282,7 @@ abstract class Sys extends \SFW\Config
          *
          * int
          */
-        $sys['dir_mode'] = 0777;
+        $config['dir_mode'] = 0777;
 
         // }}}
         // {{{ file
@@ -291,7 +291,7 @@ abstract class Sys extends \SFW\Config
          *
          * int
          */
-        $sys['file_mode'] = 0666;
+        $config['file_mode'] = 0666;
 
         // }}}
         // {{{ merger
@@ -302,25 +302,25 @@ abstract class Sys extends \SFW\Config
          *
          * ?array
          */
-        $sys['merger_sources'] = null;
+        $config['merger_sources'] = null;
 
         /* URL location with merged JS and CSS files.
          *
          * string
          */
-        $sys['merger_location'] = '/.merged';
+        $config['merger_location'] = '/.merged';
 
         /* Directory for merged JS and CSS files.
          *
          * string
          */
-        $sys['merger_dir'] = APP_DIR . '/var/cache/merged';
+        $config['merger_dir'] = APP_DIR . '/var/cache/merged';
 
         /* Cache file with merger internal data.
          *
          * string
          */
-        $sys['merger_cache'] = APP_DIR . '/var/cache/merger.php';
+        $config['merger_cache'] = APP_DIR . '/var/cache/merger.php';
 
         // }}}
         // {{{ router
@@ -329,7 +329,7 @@ abstract class Sys extends \SFW\Config
          *
          * string
          */
-        $sys['router_cache'] = APP_DIR . '/var/cache/router.php';
+        $config['router_cache'] = APP_DIR . '/var/cache/router.php';
 
         // }}}
         // {{{ paginator
@@ -338,10 +338,10 @@ abstract class Sys extends \SFW\Config
          *
          * ?string
          */
-        $sys['paginator_param'] = 'page';
+        $config['paginator_param'] = 'page';
 
         // }}}
 
-        return $sys;
+        return $config;
     }
 }
